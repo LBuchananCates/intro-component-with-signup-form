33fa47bf4cc5b0cfda2141d8a170f9839e9b717c
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2023-03-15 09:53:14 -0400

    adds js file

---
+++ signup-form.js
@@ -0,0 +1,5 @@
+// if input field left blank: display error message, display error-icon, signup button inactive
+
+// when input field filled: remove error message, remove error-icon
+
+// if all input fields filled in: activate signup button
