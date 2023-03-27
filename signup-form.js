72a7349fec52db7272c12124e69fb6a0cd6292b0
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2023-03-27 11:16:26 -0400

    error message and border become visible if 1 or more input fields left blank

--- signup-form.js
+++ signup-form.js
@@ -1,5 +1,73 @@
-// if input field left blank: display error message, display error-icon, signup button inactive
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 
-// when input field filled: remove error message, remove error-icon
+const freeTrialBtn = document.querySelector(".claim-free-trial-btn");
 
-// if all input fields filled in: activate signup button
+function showErrorBorder(submitCardInfo) {
+  if (firstName.value === "" || firstName.value == null) {
+    // turns border red
+    firstName.classList.add("error-border");
+    // display error message
+    const firstNameErrorMessage = document.getElementById(
+      "first-name-error-msg"
+    );
+    firstNameErrorMessage.style.visibility = "visible";
+    // displays error icon
+  }
+
+  if (lastName.value === "" || lastName.value == null) {
+    // turns border red
+    lastName.classList.add("error-border");
+    // display error message
+    const lastNameErrorMessage = document.getElementById("last-name-error-msg");
+    lastNameErrorMessage.style.visibility = "visible";
+    // displays error icon
+  }
+
+  if (email.value === "" || email.value == null) {
+    // turns border red
+    email.classList.add("error-border");
+    // display error message
+    const emailErrorMessage = document.getElementById("email-error-msg");
+    emailErrorMessage.style.visibility = "visible";
+    // displays error icon
+  }
+
+  if (password.value === "" || password.value == null) {
+    // turns border red
+    password.classList.add("error-border");
+    // display error message
+    const passwordErrorMessage = document.getElementById("password-error-msg");
+    passwordErrorMessage.style.visibility = "visible";
+    // displays error icon
+  }
+  // when clicking submit with errors, doesn't refresh page, displays errors
+  submitCardInfo.preventDefault();
+}
+freeTrialBtn.addEventListener("click", showErrorBorder);
+
+// upon typing, remove error class
+firstName.oninput = () => {
+  firstName.innerText = firstName.value;
+  firstName.classList.remove("error");
+};
+
+// upon typing, remove error class
+lastName.oninput = () => {
+  lastName.innerText = lastName.value;
+  lastName.classList.remove("error");
+};
+
+// upon typing, remove error class
+email.oninput = () => {
+  email.innerText = email.value;
+  email.classList.remove("error");
+};
+
+// upon typing, remove error class
+password.oninput = () => {
+  password.innerText = password.value;
+  password.classList.remove("error");
+};
